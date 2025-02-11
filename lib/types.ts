52e512f1f99eb13f913b3aaf705d97c4ee9e1642
Its-EdgarS <108824747+Its-EdgarS@users.noreply.github.com>
2025-02-10 22:15:34 -0500

    Revert "Revert "Main feed page""

---
+++ lib/types.ts
@@ -0,0 +1,18 @@
+export type Activity = {
+    id: number;
+    type: string;
+    title: string;
+    creator: string;
+    major: string;
+    year: string;
+    time: string;
+    location: string;
+    distance: string;
+    spots: number;
+    participants: number;
+    matchScore: number;
+    tags: string[];
+    aiSuggestion: string;
+    description: string;
+  };
+  
