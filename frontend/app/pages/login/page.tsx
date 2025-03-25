0d6e4cb223be73db8a08606b9524654146f88eea
Its-EdgarS <edgar.s7223@gmail.com>
2025-03-24 20:04:35 -0400

    UI adjustments to sign in and registration pages

--- frontend/app/pages/login/page.tsx
+++ frontend/app/pages/login/page.tsx
@@ -27,28 +27,36 @@ export default function Login() {
   };
 
   return (
-    <div className="bg-gray-200 h-full">
-      <h1 className="text-2xl font-bold text-red-600 pl-8 pt-8">Buckeye Connect</h1>
-      <div className="flex items-center h-screen">
-        <div className="flex items-center justify-center mx-auto bg-white p-8 rounded-lg shadow-xl">
-          <div className="mr-16 space-y-4">
-            <p>Start connecting with clubs around campus!</p>
-            <p className="font-bold">Sign In to BuckeyeConnect</p>
-            <form onSubmit={handleSubmit} className="space-y-4">
-              <div>
-                <Label htmlFor="email">Email</Label>
-                <Input type="email" id="email" placeholder="Email" value={data.email} onChange={(e) => setData({ ...data, email: e.target.value })} />
-              </div>
-              <div>
-                <Label htmlFor="password">Password</Label>
-                <Input type="password" id="password" placeholder="Password" value={data.password} onChange={(e) => setData({ ...data, password: e.target.value })} />
-              </div>
-              <Button type="submit" className="bg-red-600">Sign In</Button>
-            </form>
-            <p>Don&#39;t have an account? <a href="/pages/register" className="text-red-600">Sign Up</a></p>
-          </div>
-          <div>
-            <Image src={"/login_graphic.webp"} width={300} height={525} alt="Login Graphic" placeholder="blur" blurDataURL={'/login_graphic.webp'} />
+    <div
+      className="relative bg-center h-full"
+      style={{ backgroundImage: "url('/osu_background_small.webp')" }}
+    >
+      {/* Dark overlay */}
+      <div className="absolute inset-0 bg-black opacity-10"></div>
+
+      <div className="relative">
+        <h1 className="text-2xl font-bold text-red-600 pl-8 pt-8">BuckeyeConnect</h1>
+        <div className="flex items-center h-screen">
+          <div className="flex items-center justify-center mx-auto bg-white rounded-lg shadow-xl">
+            <div className="mr-16 space-y-4 pl-24 pr-16 text-xl">
+              <p>Start connecting with clubs around campus!</p>
+              <p className="font-bold">Sign In to BuckeyeConnect</p>
+              <form onSubmit={handleSubmit} className="space-y-6">
+                <div >
+                  <Label className="text-lg" htmlFor="email">Email</Label>
+                  <Input type="email" id="email" placeholder="Email" value={data.email} onChange={(e) => setData({ ...data, email: e.target.value })} />
+                </div>
+                <div>
+                  <Label className="text-lg" htmlFor="password">Password</Label>
+                  <Input type="password" id="password" placeholder="Password" value={data.password} onChange={(e) => setData({ ...data, password: e.target.value })} />
+                </div>
+                <Button type="submit" className="bg-red-600">Sign In</Button>
+              </form>
+              <p>Don&#39;t have an account? <a href="/pages/register" className="text-red-600">Sign Up</a></p>
+            </div>
+            <div>
+              <Image src={"/login_graphic.webp"} width={400} height={525} alt="Login Graphic" placeholder="blur" blurDataURL={'/login_graphic.webp'} className="rounded-r-lg"/>
+            </div>
           </div>
         </div>
       </div>
